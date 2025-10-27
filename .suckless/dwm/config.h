df79c81f93f9d4fe03f11fc1364b2ec2c3ee0226
Chares <chares8191@proton.me>
2025-10-27 00:09:00 -0400

    xrandr-utils updates

--- .suckless/dwm/config.h
+++ .suckless/dwm/config.h
@@ -122,6 +122,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_F9,     spawn,          SHCMD("xrandr-utils single_display_output eDP-1") },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
